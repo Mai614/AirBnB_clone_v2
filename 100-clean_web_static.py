9ece5b887118ac537796f865c78f46633bad7203
Mai614 <maymohamed614@gmail.com>
2023-10-07 07:33:42 -0700

    100web

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
 """
-a Fabric script (based on the file 3-deploy_web_static.py)
+A Fabric script to clean outdated archives locally and remotely
 """
 from fabric.api import *
 from os.path import isdir, join
-from datetime import datetime
 
 env.hosts = ['54.173.91.144', '54.157.134.6']
 
+def do_local_clean(number=0):
+    """
+    Deletes out-of-date archives locally
+    """
+    number = int(number) + 1
+    local_archives = sorted(run('ls -1t versions').split())
+    for archive in local_archives[number:]:
+        local('rm versions/{}'.format(archive))
+    print("Local: Old archives deleted!")
+
+def do_remote_clean(number=0):
+    """
+    Deletes out-of-date archives remotely
+    """
+    number = int(number) + 1
+    with hide('running', 'output', 'commands'):
+        remote_archives = sorted(run('ls -1t /data/web_static/releases').split())
+    for archive in remote_archives[number:]:
+        run('rm -rf /data/web_static/releases/{}'.format(archive))
+    print("Remote: Old archives deleted!")
+
 def do_clean(number=0):
     """
-    Deletes out-of-date archives
+    Deletes out-of-date archives both locally and remotely
     """
-    number = int(number)
-    if number < 1:
-        number = 1
+    do_local_clean(number)
+    do_remote_clean(number)
 
-    try:
-        with hide('running', 'output', 'commands'):
-            archives = sorted(run('ls -1t versions').split())
+if __name__ == "__main__":
+    do_clean()
 
-        for archive in archives[number:]:
-            run('rm versions/{}'.format(archive))
-
-        with hide('running', 'output', 'commands'):
-            releases = sorted(run('ls -1t /data/web_static/releases').split())
-
-        for release in releases[number:]:
-            run('rm -rf /data/web_static/releases/{}'.format(release))
-
-        print("Old archives and releases deleted!")
-        return True
-    except Exception as e:
-        print(e)
-        return False

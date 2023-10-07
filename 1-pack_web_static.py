14df12afdcfd617fde20c4fe380b855e69b3a056
Mai614 <maymohamed614@gmail.com>
2023-10-07 06:37:33 -0700

    1111

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- Fabric script that generates a .tgz archive from the contents
+Fabric script that generates a .tgz archive from the contents
 """
 
 from fabric.api import local
@@ -10,6 +10,9 @@ import os
 def do_pack():
     """
     Generates a .tgz archive from the contents of the web_static folder.
+
+    Returns:
+        str: Archive path if successful, None otherwise.
     """
     try:
         if not os.path.exists("versions"):
@@ -18,9 +21,13 @@ def do_pack():
         now = datetime.now()
         archive_name = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-        local("tar -czvf versions/{} web_static".format(archive_name))
+        # Use capture to handle the output and errors properly
+        result = local("tar -czvf versions/{} web_static".format(archive_name), capture=True)
 
-        return "versions/{}".format(archive_name)
+        if result.succeeded:
+            return "versions/{}".format(archive_name)
+        else:
+            return None
     except Exception as e:
         print("Error: {}".format(e))
         return None

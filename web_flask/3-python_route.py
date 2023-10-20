7e44e797497419f45e46f5bb6423070a33ea4afe
Mai614 <maymohamed614@gmail.com>
2023-10-20 14:34:11 +0000

    route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Script that starts a Flask web application
 """
@@ -10,20 +9,32 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route that returns a message when accessed
+    """
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    Route that returns a specific message when accessed
+    """
     return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """
+    Route that takes a parameter and returns a message
+    """
     text = escape(text).replace('_', ' ')
     return "C {}".format(text)
 
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
-def python_text(text='is cool'):
+def python_text(text):
+    """
+    Route that takes an optional parameter and returns a message
+    """
     text = escape(text).replace('_', ' ')
     return "Python {}".format(text)
 

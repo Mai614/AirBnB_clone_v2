d87b28cd0de7729487a19e76a63044c393c04a59
Mai614 <maymohamed614@gmail.com>
2023-10-22 17:50:50 +0000

    1route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""
+Script that starts a Flask web application
+"""
 
 from flask import Flask
 
@@ -16,4 +18,3 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-

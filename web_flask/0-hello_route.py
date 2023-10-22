ce14a6ae70d35356846c72cc4042452c3a98966c
Mai614 <maymohamed614@gmail.com>
2023-10-22 18:22:52 +0000

    0-hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -19,22 +19,6 @@ def hello():
 
 # The following block checks if the script is being run directly. If it is,
 # the Flask development server is started, listening on all network interfaces
-# (host='0.0.0.0') and port 5000. This allows the web application to be accessible
-# externally.
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-#!/usr/bin/python3
-"""
-Script that starts a Flask web application
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
